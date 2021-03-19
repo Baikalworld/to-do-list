a6f3a0d26d6426c55a40ab9b2e40c02d4c74b3c9
Roberto Ubieto Juan <robertouj@gmail.com>
2021-03-19 22:26:56 +0100

    created first component

---
+++ src/Components/InputButton/InputButton.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+// this is a component
+function InputButton() {
+  return (
+    <>
+      <input type="text" className="InputButton" />
+      <button className="add">add</button>
+    </>
+  );
+}
+
+export default InputButton;
