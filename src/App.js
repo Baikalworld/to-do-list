d648b602eb5a975ae08064ea3467dd2a49edd0c9
Roberto Ubieto Juan <robertouj@gmail.com>
2021-03-23 21:58:26 +0100

    add functionality finished

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputButton from "./Components/InputButton";
 import TaskList from "./Components/TaskList";
 
 function App() {
-  const tasks = [
-    {
-      text: "task",
-      status: "in progress"
-    },
-    {
-      text: "task 2",
-      status: "in progress"
-    }
-  ];
+  const [tasks, setTasks] = useState(
+    [
+      {
+        text: "task",
+        status: "in progress",
+      },
+      {
+        text: "task 2",
+        status: "in progress",
+      },
+      {
+        text: "task 3",
+        status: "in progress",
+      },
+    ]
+  );
+
+  const addNewTask = (text) => {
+    const tasksAux = [
+      ...tasks,
+      {
+        text: text,
+        status: "in progress"
+      }
+    ];
+
+    setTasks(tasksAux);
+    // bla bla bla
+  };
 
   return (
     <div className="App">
       <h1>To Do List Group 2 </h1>
-      <InputButton />
+      <InputButton addNewTask={addNewTask} />
       <TaskList tasks={tasks} />
     </div>
   );
--- src/Components/InputButton/InputButton.js
+++ src/Components/InputButton/InputButton.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 
-// this is a component 
-// just to add Tasks
-function InputButton() {
+// this is a component
+// just to add Task
+function InputButton({ addNewTask }) {
+  const inputEl = useRef(null);
+
+  const onClick = () => {    
+    addNewTask(inputEl.current.value);
+    inputEl.current.value = "";
+  };
+
   return (
     <>
-      <input type="text" className="InputButton" />
-      <button className="add">Add</button>
+      <input ref={inputEl} type="text" className="InputButton" />
+      <button onClick={onClick} className="add">
+        Add
+      </button>
     </>
   );
 }
